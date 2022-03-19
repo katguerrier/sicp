55d5441474eddc51f5bfc41e713ccc941df8c653
Katrina Guerrier <guerrier.katrina@gmail.com>
2022-03-19 18:12:49 -0400

    refactoring factorial - ha - to show two different processes, one recursive and one iterative

--- factorial.py
+++ factorial.py
@@ -1,5 +1,15 @@
-def factorial(n):
+# recursive factorial process
+# The recursive factorial process is intuitive but computationally more intensive and will hit against recursion depth limits.
+def recFactorial(n):
     if (n > 1):
       return (n * factorial(n-1))
     else:
         return 1
+    
+# iterative factorial process
+# The iterative factorial process has only the product, counter, and endpoint to keep in memory at any given time. 
+def iterFactorial(n):
+    product = 1
+    for c in range(1,n+1):
+        product *= c
+    return product
